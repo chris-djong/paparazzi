77414273de36a4d00507046bdf53cc67f3f837e0
cpldejong <djchris261@hotmail.com>
2019-11-23 11:29:52 +0100

    Updating file logger

--- sw/airborne/modules/loggers/file_logger.c
+++ sw/airborne/modules/loggers/file_logger.c
@@ -92,7 +92,7 @@ void file_logger_start(void)
 #ifdef COMMAND_THRUST
       "counter,gyro_unscaled_p,gyro_unscaled_q,gyro_unscaled_r,accel_unscaled_x,accel_unscaled_y,accel_unscaled_z,mag_unscaled_x,mag_unscaled_y,mag_unscaled_z,COMMAND_THRUST,COMMAND_ROLL,COMMAND_PITCH,COMMAND_YAW,qi,qx,qy,qz\n"
 #else
-      "counter,gyro_unscaled_p,gyro_unscaled_q,gyro_unscaled_r,accel_unscaled_x,accel_unscaled_y,accel_unscaled_z,mag_unscaled_x,mag_unscaled_y,mag_unscaled_z,	h_ctl_aileron_setpoint, h_ctl_elevator_setpoint, desired_ground_speed, actual_enu_speed, v_ctl_auto_cruise_throttle, dist_wp_follow, ground_pos_utm_x, ground_pos_utm_y, ground_pos_utm_z, uav_pos_utm_x, uav_pos_utm_y, uav_pos_utm_z, wind_x, wind_y, wind_z, airspeed, angle_attack, sideslip, GPSstateGS, GPSstateAC\n"
+      "counter,gyro_unscaled_p,gyro_unscaled_q,gyro_unscaled_r,accel_unscaled_x,accel_unscaled_y,accel_unscaled_z,mag_unscaled_x,mag_unscaled_y,mag_unscaled_z,gyro_p,gyro_q,gyro_r,accel_x,accel_y,accel_z,mag_x,mag_y,mag_z,h_ctl_aileron_setpoint, h_ctl_elevator_setpoint, desired_ground_speed, actual_enu_speed, v_ctl_auto_cruise_throttle, dist_wp_follow, ground_pos_utm_x, ground_pos_utm_y, ground_pos_utm_z, uav_pos_utm_x, uav_pos_utm_y, uav_pos_utm_z, wind_x, wind_y, wind_z, airspeed, angle_attack, sideslip, GPSstateGS, GPSstateAC\n"
 #endif
     );
   }
@@ -104,7 +104,7 @@ void file_logger_stop(void)
   if (file_logger != NULL) {
     fclose(file_logger);
     file_logger = NULL;
-  }
+    }
 }
 
 /** Log the values to a csv file    */
@@ -143,7 +143,7 @@ void file_logger_periodic(void)
           quat->qz
          );
 #else  // For fixedwing
-  fprintf(file_logger, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d\n",
+  fprintf(file_logger, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%d,%d\n",
           counter, // int
           imu.gyro_unscaled.p, // int
           imu.gyro_unscaled.q, // int
@@ -154,6 +154,15 @@ void file_logger_periodic(void)
           imu.mag_unscaled.x, // int
           imu.mag_unscaled.y, // int
           imu.mag_unscaled.z, // int
+		  imu.gyro_unscaled.p, // int
+		  imu.gyro.q, // int
+		  imu.gyro.r, // int
+		  imu.accel.x, // int
+		  imu.accel.y, // int
+		  imu.accel.z, // int
+		  imu.mag.x, // int
+		  imu.mag.y, // int
+	      imu.mag.z, // int
 		  h_ctl_aileron_setpoint, // int
 		  h_ctl_elevator_setpoint, // int
 		  ground_speed, // float
